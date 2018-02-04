3921aece77d890cdace19ce3bd60f3f4caa40c1b
Darren Britton <darrenbritton@hotmail.com>
2018-02-04 22:52:41 +0000

    added fetch polyfill to make component more compatible

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import buildUrl from 'build-url'
 import Gallery from './components/gallery'
+import 'whatwg-fetch'
 
 
 /**
