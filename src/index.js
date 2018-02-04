b07b62d11b17d5d4b07c1315af6995c3ea67c982
Darren Britton <darrenbritton@hotmail.com>
2018-02-04 23:02:07 +0000

    migrated to node-fetch

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import buildUrl from 'build-url'
 import Gallery from './components/gallery'
-import 'whatwg-fetch'
+import fetch from 'node-fetch'
 
 
 /**
